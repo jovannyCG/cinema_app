92b5be84cb88383d60c701754f13945e46905052
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-05-06 13:12:40 -0600

    mapeo de respuesta de moviedb terminado

--- lib/infrastructure/datasources/moviesdb_datasource.dart
+++ lib/infrastructure/datasources/moviesdb_datasource.dart
@@ -4,6 +4,8 @@
 import 'package:cinema_app/config/constants/enviroment.dart';
 import 'package:cinema_app/domain/datasources/movies_datasource.dart';
 import 'package:cinema_app/domain/entities/movie.dart';
+import 'package:cinema_app/infrastructure/datasources/models/moviedb/moviedb_response.dart';
+import 'package:cinema_app/infrastructure/mappers/movie_mapper.dart';
 import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends  MoviesDatasource{
@@ -20,9 +22,18 @@ class MoviedbDatasource extends  MoviesDatasource{
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async{
+    //peticion http
     final resp = await dio.get('/movie/now_playing');
-    final List<Movie> movies =[];
-    return[];
+    //almacenamiento de la respuesta json de tipp moviedb
+    final moviedbResponse = MovieDbResponse.fromJson(resp.data);
+    // mapeo de la respuesta para convertirla a una respuesta de tipo movie usando el mapper
+    final List<Movie> movies =moviedbResponse.results
+    .where((moviedb) => moviedb.posterPath != 'no-poster')
+    .map(
+      (moviedb) => MovieMapper.movieDBToEntity(moviedb)
+  ).toList();
+
+    return movies;
   }
 
   
--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -17,7 +17,7 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: (moviedb.posterPath != '')
       ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-      : 'https://www.gmt-sales.com/wp-content/uploads/2015/10/no-image-found.jpg',
+      : 'no poster',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
