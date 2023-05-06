45589593673e7899bbf1dc1e42361794a7605284
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-05-06 14:02:55 -0600

    configuración de provider y notifier para now playing

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,26 @@
+import 'package:cinema_app/domain/entities/movie.dart';
+import 'package:cinema_app/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nowPlayingMoviesProvider =
+    StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+      final fechMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
+  return MoviesNotifier(fechMoreMovies: fechMoreMovies);
+});
+
+typedef MovieCallback = Future <List<Movie>> Function({int page});
+
+class MoviesNotifier extends StateNotifier<List<Movie>> {
+  int currentPage = 0;
+MovieCallback fechMoreMovies;
+
+  MoviesNotifier({
+    required this.fechMoreMovies,
+  }) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+    final List<Movie> movies = await fechMoreMovies(page: currentPage);
+    state =[...state, ...movies];
+  }
+}
