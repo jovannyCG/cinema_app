614b3d82cd7cc3fb3541be2d56bf2cbc03b95d67
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-05-06 10:21:28 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:cinema_app/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      theme: AppTheme().getTheme(),
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
