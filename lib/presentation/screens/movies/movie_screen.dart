81c29822f2f30d2760b3ea4e4f4b535a0f289f08
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-05-16 10:20:54 -0600

    incio del diseño de pantalla de pelicula

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MovieScreen extends ConsumerStatefulWidget {
-  static const name ='movie-screen'; 
+  static const name = 'movie-screen';
 
   final String movieId;
   const MovieScreen({super.key, required this.movieId});
@@ -18,21 +18,80 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
   void initState() {
     super.initState();
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
-    
   }
+
   @override
   Widget build(BuildContext context) {
     final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
-    if(movie ==null){
+    if (movie == null) {
       return const Scaffold(
         body: Center(
-          child: CircularProgressIndicator(strokeWidth: 3,),
+          child: CircularProgressIndicator(
+            strokeWidth: 3,
+          ),
         ),
       );
     }
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text('data ${widget.movieId}'),
+    return Scaffold(
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: <Widget>[
+          _SliverAppBarMovie(
+            movie: movie,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _SliverAppBarMovie extends StatelessWidget {
+  final Movie movie;
+  const _SliverAppBarMovie({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7,
+      foregroundColor: Colors.white,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+        title: Text(
+          movie.title,
+          style: const TextStyle(fontSize: 20),
+          textAlign: TextAlign.justify,
+        ),
+        background: Stack(children: [
+          SizedBox.expand(
+            child: Image.network(
+              movie.posterPath,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox.expand(
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [0.7, 1.0],
+                        colors: [Colors.transparent, Colors.black87]))),
+          ),
+          const SizedBox.expand(
+              child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      gradient:
+                          LinearGradient(begin: Alignment.topLeft, stops: [
+            0.0,
+            0.4
+          ], colors: [
+            Colors.black87,
+            Colors.transparent,
+          ])))),
+        ]),
       ),
     );
   }
