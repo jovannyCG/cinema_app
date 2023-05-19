90fdb8fbf7230f3e46fb8da1f46405398bd09622
Jovanny Carlos Gonzalez <jovannycarlosgonzalez9@gmail.com>
2023-05-19 11:56:22 -0600

    leading y actions de search delegate terminadas

--- lib/presentation/delegates/search_delegate_movie.dart
+++ lib/presentation/delegates/search_delegate_movie.dart
@@ -1,19 +1,32 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinema_app/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 //implementacipon de las busquedas
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?>{
   //cambiar la frase que aparece al abrir la busqueda
   @override
   String get searchFieldLabel => 'Buscar película';
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [const Text('buildActions')];
+    return [
+     //if(query.isNotEmpty)
+      FadeIn(
+        animate: query.isNotEmpty,
+        duration: const Duration(milliseconds: 500),
+        child: IconButton(onPressed: ()=> query ='', 
+        icon: const Icon(Icons.clear)),
+      )
+    ];
   }
 
-//este metodo construye un icono que aparece al realizar la busqueda
+//este metodo construye un icono/boton que aparece al realizar la busqueda, ese icono hace una accion espesifíca
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(onPressed: (){
+      close(context, null);
+
+    }, icon: const Icon(Icons.arrow_back_outlined));
   }
 
   //este metodo construye los resultados de la buqueda
